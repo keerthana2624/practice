1dda92014b2cba86cf3fd8a79b244b60ea7e38b0
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-03-05 22:40:16 +0530

    div method called in Main

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Test test = new Test();
+        int result = test.division(6, 3);
+        System.out.println(result);
+
+    }
+
+}
