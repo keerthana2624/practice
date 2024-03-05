2780bd6eef4b955dd11d0067e859f708dce74667
gowtham1035 <gowthamneelam1@msitprogram.net>
2024-03-05 22:44:31 +0530

    Even or not function updated.

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        Test med = new Test();
+        System.out.println(med.evenOrNot(5));
+    }
+}
--- Test.java
+++ Test.java
@@ -1,3 +1,10 @@
 public class Test {
-    
+
+    public boolean evenOrNot(double element){
+        if(element%2 == 0){
+            return true;
+        }
+        return false;
+
+    }
 }
