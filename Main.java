1ecb8832f2603bed76bc4a91b84ccd8217fd6a13
kolikondasushmitha <sushmithaanju12@gmail.com>
2024-03-05 22:47:49 +0530

    i have implement pro method

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
   public static void main(String[] args){
     
+    Test obj= new Test();
+    int x =obj.pro(2, 5);
+    System.out.println(x);
   }
 }
--- Test.java
+++ Test.java
@@ -1,3 +1,7 @@
 public class Test {
-    
+
+    public int pro(int a , int b)
+{
+    return a*b;
+}    
 }
