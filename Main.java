1055c9ef864e93748cd9482e20601f01dccdfc90
keerthana2624 <keerthanamummy2002@gmail.com>
2024-03-05 22:32:42 +0530

    add function is implimented

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        Test test=new Test();
+        test.add(2,6);
+        test.add(4,3);
+
+    }
+}
--- Test.java
+++ Test.java
@@ -1,3 +1,5 @@
 public class Test {
-    
+    public int add(int a, int b){
+        return a+b;
+    }
 }
